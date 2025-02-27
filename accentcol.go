94f7438da0956f7c195ef7a4a9c08615c1df3e6d
Aegiscarr <aegiscarr@gmail.com>
2025-02-27 23:21:51 +0100

    barebones for new command router (ken)

--- accentcol.go
+++
@@ -1,5 +0,0 @@
-package main
-
-type DBSettings struct {
-	Color int
-}
--- cumulonimbus.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type CumulonimbusResponse struct {
-	URL       string
-	Manage    string
-	Thumbnail string
-}
