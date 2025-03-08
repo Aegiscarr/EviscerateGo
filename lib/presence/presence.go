4562067f279e0a962ad08144423d5d24b841229d
Aegiscarr <aegiscarr@gmail.com>
2025-03-08 21:45:57 +0100

    whoops forgot to disable buildstring

--- lib/presence/presence.go
+++ lib/presence/presence.go
@@ -2,12 +2,12 @@ package presence
 
 import "github.com/bwmarrin/discordgo"
 
-var buildstring string = "b250308-ken"
+//var buildstring string = "b250308-ken"
 var err error
 
 func SetStatusOnLaunch(s *discordgo.Session) error {
 	err = s.UpdateStatusComplex(discordgo.UpdateStatusData{
-		Activities: []*discordgo.Activity{{Type: 3, Name: "over the Den // " + buildstring}},
+		Activities: []*discordgo.Activity{{Type: 3, Name: "over the Den"}},
 	})
 	return err
 }
